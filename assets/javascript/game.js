88aa4e3274cebdbccbf7ef3475cba11d2d192111
enigma <deepaul.bharj@gmail.com>
2016-11-05 12:09:43 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,86 @@
+// Variables
 
-// computer guesses
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Create an array that lists out all of the options
+var wins = 0;
+var losses = 0;
+var guessesleft = 0;
+var guessessofar = 0;
+var wrongGuessArray = [];
+var answerArray = [];
 
-var wins = 0
-var losses = 0
-var guessesleft = 0
-var guessessofar = 0
+var random = Math.random();
+var indexRandom = random * computerChoices.length;
+var flooredNumber = Math.floor(indexRandom);
+var randomLetters = computerChoices[flooredNumber];
 
+//Push data into wrong array, use the array to push data into html create function for user guess to push into wrong array.
 
-
-// user presses letter key
+// Functions
 
 document.onkeyup = function(event) {
 
+  if (userGuess == computerGuess){
+    wins++;
+    guessessofar++;
+    guessessleft++;
+  }
+  else {
+        losses++;
+        guessesleft++;
+        guessessofar++;
+    }
+
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
 
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  console.log(userGuess);
 
-// Sets the computer guess variable to select random characters
-  var computerGuess = guessesLeft[Math.floor(Math.random()*guessesLeft.length)];
-  console.log(computerGuess);
+  for (var i = 0; i <randomLetters.length; i++){
+    if (userGuess === randomLetters){
+      wins++
+      guessesLeft--;
+    } else{
+      losses++
+      guessesLeft--;
+    }
+
+  }
+
+  var wrongGuessArray;
+  computerChoices.push("userGuess");
+  console.log ("userGuess = " + userGuess);
+
+
+ if (wrongGuessArray.indexOf(userGuess) === -1) {
+      if (answerArray.indexOf(userGuess) === -1) {
+          (wrongGuessArray.push(guessessofar) === -1)
+            guessessofar++;
+            console.log("guessessofar = " + guessessofar);
+          }
+        }
+  // Setsthe computer guess variable to select random characters
 
 // // if letter is the same as comp letter, user wins / win counter increases
-document.querySelector("#wins").innerHTML= "wins:" + wins;
-// // if letter is different than comp leter, user loses / // loss counter increases
-document.querySelector("#loses").innerHTML= "loses" + loses;
-// guess is recorded in "your guesses so far"
-document.querySelector("#guessesleft").innerHtml= "guessesleft" + computerChoices;
-// "guesses left" is populated
-document.querySelector("#guessessofar").innerHTML= "guessessofar:" + guessessofar;
 
 
-var computerGuess = computerChoices [Math.floor(Math.random( * computerChoices.length)];
-
-
+function updateStuff(){
+  document.getElementById("wins").innerHTML= "wins " + wins;
+  // // if letter is different than comp leter, user loses / // loss counter increases
+  document.getElementById("losses").innerHTML= "losses " + losses;
+  // guess is recorded in "your guesses so far"
+  document.getElementById("guessesleft").innerHTML= "computerChoices";
+  // "guesses left" is populated
+  document.getElementById("guessessofar").innerHTML= "userGuess " + computerGuess;
+  document.getElementById("guessesRemaining").innerHTML= answerArray;
 };
+
+
+
+
+
+
+};//end keypress
+
 
 // after all guesses are gone, restart game
